9327b38c203b9453c93843bbab9329c33ff89160
electrical-rick <erick.augusto.ebs@gmail.com>
2021-06-12 00:27:01 -0300

    Modificação no bash

--- cln_process.sh
+++ cln_process.sh
@@ -1,15 +1,9 @@
 echo 'Este processo deletará os arquivos que forem colocados como parâmetro'
 echo 'Digite a extensão dos arquivos que deseja destruir: '
 read ARQ
-function delete_ext(a) {
-	while true;
-		do rm ./*$a; 
-		break;
-		if [[ $a == false ]];
-			then break;
-		fi
-	done
+while true;
+do rm ./*$ARQ;
+break;
+done;
 
-}
-
-delete_ext(ARQ)
+echo 'Feito!'
